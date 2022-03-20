61c67bb33ec368524de037412a413cc0b48da31d
Ashley Richard <ashleymrichard@gmail.com>
2022-03-20 00:15:55 -0700

    x box fix

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 const $textInput = $("[type = 'text']")
 const $submitInput = $("[type = 'submit']")
 const $ul = $("#this-list")
+const $redBut = $('<input type="button" value="X"/>')
 
 const skills = []
 
@@ -16,8 +17,20 @@ $submitInput.on("click", (event) => {
   $li.text(newSkill)
   $ul.append($li)
 
-  $('<div/ class = "box-red">X</div>').appendTo('.these-items');
-  $('div.box-red').fadeIn().css('display', 'inline');
+  // $('<div/ class = "box-red">X</div>').appendTo('.these-items');
+  // $('div.box-red').fadeIn().css('display', 'inline');
+
+  // $(function(){
+  //   $('button').on('click',function(){
+  //       // let redBut = $('<input type="button" value="X"/>');
+  //       $("ul").append($redBut);
+  //   });
+  // });
+
+  $(document).ready(function() {
+    $("ul").addClass("redBut");
+    $("ul > li").prepend($redBut);
+  });
 
   const remove = (event) => {
     const $target = $(event.target)
